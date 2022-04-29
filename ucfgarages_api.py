63278060dc4a7d9d4e01737d436c0a75f19bbb53
Jake <jakev224@gmail.com>
2022-04-29 21:04:50 +0200

    Cleaning of the folder and fix script to allow Heroku deployment and local deployment (#2)
    * Add files needed for heroku deployment
    
    * Add all clean files for heroku deployment and local use
    
    * Close connection of db in scraper script
    
    * Update requirements.txt

--- ucfgarages_api.py
+++ ucfgarages_api.py
@@ -4,6 +4,7 @@ import mysql.connector
 from fastapi import FastAPI, Response
 
 from query_database import *
+from get_env_variables import *
 
 app = FastAPI()
 
@@ -21,9 +22,14 @@ my_cursor = my_database.cursor()
 # all data based on parameters to return as json file.
 @app.get("/all")
 async def get_alldb_data():
+    my_database = connect_to_db()
+    my_cursor = my_database.cursor()
     Object_Query = DataQueryExtractor(my_database, my_cursor)
+    content = Object_Query.get_all_data()
+    my_cursor.close()
+    my_database.close()
     return Response(content=json.dumps(
-        Object_Query.get_all_data(),
+        content,
         ensure_ascii=False,
         allow_nan=False,
         indent=3,
@@ -34,9 +40,33 @@ async def get_alldb_data():
 # all today data  to return as json file.
 @app.get("/today")
 async def get_todaydb_data():
+    my_database = connect_to_db()
+    my_cursor = my_database.cursor()
     Object_Query = DataQueryExtractor(my_database, my_cursor)
+    content = Object_Query.get_today_data()
+    my_cursor.close()
+    my_database.close()
     return Response(content=json.dumps(
-        Object_Query.get_today_data(),
+        content,
+        ensure_ascii=False,
+        allow_nan=False,
+        indent=3,
+        separators=(", ", ": "),
+    ).encode("utf-8"), status_code=200, media_type="application/json")
+
+
+
+# Weekly data to return as json file.
+@app.get("/")
+async def get_last_data():
+    my_database = connect_to_db()
+    my_cursor = my_database.cursor()
+    Object_Query = DataQueryExtractor(my_database, my_cursor)
+    content = Object_Query.get_last_data()
+    my_cursor.close()
+    my_database.close()
+    return Response(content=json.dumps(
+        content,
         ensure_ascii=False,
         allow_nan=False,
         indent=3,
@@ -45,11 +75,16 @@ async def get_todaydb_data():
 
 
 # Weekly data to return as json file.
-@app.get("/")
+@app.get("/week")
 async def get_weekly_data():
+    my_database = connect_to_db()
+    my_cursor = my_database.cursor()
     Object_Query = DataQueryExtractor(my_database, my_cursor)
+    content = Object_Query.get_last_week_data()
+    my_cursor.close()
+    my_database.close()
     return Response(content=json.dumps(
-        Object_Query.get_last_week_data(),
+        content,
         ensure_ascii=False,
         allow_nan=False,
         indent=3,
@@ -60,9 +95,14 @@ async def get_weekly_data():
 # yearly data based on parameter to return as json file.
 @app.get("/{year}")
 async def get_year_data(year):
+    my_database = connect_to_db()
+    my_cursor = my_database.cursor()
     Object_Query = DataQueryExtractor(my_database, my_cursor)
+    content = Object_Query.get_yearly_data(year)
+    my_cursor.close()
+    my_database.close()
     return Response(content=json.dumps(
-        Object_Query.get_yearly_data(year),
+        content,
         ensure_ascii=False,
         allow_nan=False,
         indent=3,
@@ -73,9 +113,14 @@ async def get_year_data(year):
 # yearly monthly data based on parameters to return as json file.
 @app.get("/{year}/{month}")
 async def get_year_month_data(year, month):
+    my_database = connect_to_db()
+    my_cursor = my_database.cursor()
     Object_Query = DataQueryExtractor(my_database, my_cursor)
+    content = Object_Query.get_yearly_monthly_data(year, month)
+    my_cursor.close()
+    my_database.close()
     return Response(content=json.dumps(
-        Object_Query.get_yearly_monthly_data(year, month),
+        content,
         ensure_ascii=False,
         allow_nan=False,
         indent=3,
@@ -86,9 +131,14 @@ async def get_year_month_data(year, month):
 # yearly monthly daily data based on parameters to return as json file.
 @app.get("/{year}/{month}/{day}")
 async def get_year_month_day_data(year, month, day):
+    my_database = connect_to_db()
+    my_cursor = my_database.cursor()
     Object_Query = DataQueryExtractor(my_database, my_cursor)
+    content = Object_Query.get_yearly_monthly_daily_data(year, month, day)
+    my_cursor.close()
+    my_database.close()
     return Response(content=json.dumps(
-        Object_Query.get_yearly_monthly_daily_data(year, month, day),
+        content,
         ensure_ascii=False,
         allow_nan=False,
         indent=3,
