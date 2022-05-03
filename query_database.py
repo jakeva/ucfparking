fa70a027f8e63d7c63091a4a8bd3472ea1772c7c
Jake <jakev224@gmail.com>
2022-05-02 22:00:29 -0400

    Fix formatting

--- query_database.py
+++ query_database.py
@@ -1,7 +1,7 @@
 import datetime
 
 
-def row_formatting(data,cnt):
+def row_formatting(data, cnt):
     date_of_row = data[0]
 
     garage_row_queried = {
@@ -12,25 +12,20 @@ def row_formatting(data,cnt):
     cnt += 1
 
     for garage_number in range(7):
-        formatted_dict_for_row_garage_number = {'max_spaces': data[3*garage_number+2], 'name': "Garage A",
-                                                'percent_full': data[3*garage_number+3], 'spaces_filled': int(
-                                                int(data[3*garage_number+2]) - int(data[3*garage_number+1])),
-                                                'spaces_left': data[3*garage_number+1]}
+        formatted_dict_for_row_garage_number = {'max_spaces': data[3 * garage_number + 2], 'name': "Garage A",
+                                                'percent_full': data[3 * garage_number + 3], 'spaces_filled': int(
+                int(data[3 * garage_number + 2]) - int(data[3 * garage_number + 1])),
+                                                'spaces_left': data[3 * garage_number + 1]}
 
         garage_row_queried['garages'].append(formatted_dict_for_row_garage_number)
 
-
     return garage_row_queried, cnt
-
-
 
 
 class DataQueryExtractor:
     def __init__(self, my_database, my_cursor):
         self.my_database = my_database
         self.my_cursor = my_cursor
-
-
 
     def get_last_week_data(self):
         week_ago_date = datetime.datetime.now() - datetime.timedelta(days=7)
@@ -44,13 +39,12 @@ class DataQueryExtractor:
 
         cnt = 0
         for data in self.my_cursor:
-            garage_row_queried, cnt = row_formatting(data,cnt)
+            garage_row_queried, cnt = row_formatting(data, cnt)
             weekly_data_queried['data'].append(garage_row_queried)
 
         weekly_data_queried['count'] = cnt
 
         return weekly_data_queried
-
 
     def get_last_data(self):
         query = "SELECT MAX(date_and_time) FROM parking_data"
@@ -58,18 +52,14 @@ class DataQueryExtractor:
         for date in self.my_cursor:
             max_date = date[0]
 
-
         query = "SELECT * FROM parking_data WHERE date_and_time = %s"
         self.my_cursor.execute(query, [max_date])
 
         cnt = 0
         for data in self.my_cursor:
-            garage_row_queried, cnt = row_formatting(data,cnt)
+            garage_row_queried, cnt = row_formatting(data, cnt)
 
         return garage_row_queried
-
-
-
 
     def get_yearly_data(self, year):
 
@@ -83,14 +73,12 @@ class DataQueryExtractor:
 
         cnt = 0
         for data in self.my_cursor:
-            garage_row_queried, cnt = row_formatting(data,cnt)
+            garage_row_queried, cnt = row_formatting(data, cnt)
             yearly_data_queried['data'].append(garage_row_queried)
 
         yearly_data_queried['count'] = cnt
 
         return yearly_data_queried
-
-
 
     def get_yearly_monthly_data(self, year, month):
 
@@ -104,14 +92,12 @@ class DataQueryExtractor:
 
         cnt = 0
         for data in self.my_cursor:
-            garage_row_queried, cnt = row_formatting(data,cnt)
+            garage_row_queried, cnt = row_formatting(data, cnt)
             yearly_monthly_data_queried['data'].append(garage_row_queried)
 
         yearly_monthly_data_queried['count'] = cnt
 
         return yearly_monthly_data_queried
-
-
 
     def get_yearly_monthly_daily_data(self, year, month, the_day):
 
@@ -125,15 +111,12 @@ class DataQueryExtractor:
 
         cnt = 0
         for data in self.my_cursor:
-            garage_row_queried, cnt = row_formatting(data,cnt)
+            garage_row_queried, cnt = row_formatting(data, cnt)
             yearly_monthly_daily_data_queried['data'].append(garage_row_queried)
 
         yearly_monthly_daily_data_queried['count'] = cnt
 
         return yearly_monthly_daily_data_queried
-
-
-
 
     def get_all_data(self):
 
@@ -147,15 +130,12 @@ class DataQueryExtractor:
 
         cnt = 0
         for data in self.my_cursor:
-            garage_row_queried, cnt = row_formatting(data,cnt)
+            garage_row_queried, cnt = row_formatting(data, cnt)
             all_data_queried['data'].append(garage_row_queried)
 
         all_data_queried['count'] = cnt
 
         return all_data_queried
-
-
-
 
     def get_today_data(self):
         today_date = datetime.datetime.now()
@@ -173,7 +153,7 @@ class DataQueryExtractor:
 
         cnt = 0
         for data in self.my_cursor:
-            garage_row_queried, cnt = row_formatting(data,cnt)
+            garage_row_queried, cnt = row_formatting(data, cnt)
             all_today_data_queried['data'].append(garage_row_queried)
 
         all_today_data_queried['count'] = cnt
