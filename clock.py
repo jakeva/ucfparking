4ffc0b5deb1673c99eff658d016a85d6f881f31a
Jake <jakev224@gmail.com>
2022-05-02 21:59:06 -0400

    Schedule job correctly to run every sharp hour

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ from main import main
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', hour='*')
+@sched.scheduled_job('cron', hour='*', minute=0, second=0)
 def timed_job():
     main()
 
