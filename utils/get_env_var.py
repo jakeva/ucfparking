d6ae8432a86dcfb8f1884b0b841241e8ad26dcc9
Thomas Delsart <thomas.dlsrt@gmail.com>
2022-07-25 22:48:06 +0200

    Add pre-commit for cleaning, clean utils/ folder. Do not consider type annotation for cleaning.

--- utils/get_env_var.py
+++ utils/get_env_var.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-if os.environ.get("DB_PASS") is None and os.path.exists(os.path.join(ROOT_DIR, '../.env')):
-    load_dotenv(os.path.join(ROOT_DIR, '../.env'))
+if os.environ.get("DB_PASS") is None and os.path.exists(os.path.join(ROOT_DIR, "../.env")):
+    load_dotenv(os.path.join(ROOT_DIR, "../.env"))
--- utils/query_database.py
+++ utils/query_database.py
@@ -1,35 +1,15 @@
 """Script that defines all functions to query data from the database."""
 import datetime
+import os
+
 import mysql.connector
-from utils.get_env_var import *
-
-class Database:
-    def __init__(self):
-        self.database = mysql.connector.connect(
-        host=os.environ['DB_HOST'],
-        port=os.environ['DB_PORT'],
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASS'],
-        database=os.environ['DB_NAME']
-    )
-        self.cursor = self.database.cursor()
-
-    def setup_query_extractor(self):
-        return DataQueryExtractor(self.database, self.cursor)
-
-    def update_database(self, query, data):
-        self.cursor.execute(query, data)
-        self.database.commit()
-
-    def close_connection(self):
-        self.database.close()
-        self.cursor.close()
+from utils.get_env_var import *  # noqa F403
 
 
-
-def row_formatting(data, cnt: int):
+def row_formatting(data, cnt):
     """
-    Formats the data to be inserted into the database to create a new row.
+    Format the data to be inserted into the database to create a new row.
+
     :param data: The data to be formatted.
     :param cnt: number of data points in the database
     :return: formatted data and the cnt value
@@ -37,254 +17,309 @@ def row_formatting(data, cnt: int):
     date_of_row = data[0]
 
     garage_row_queried = {
-        'date_and_time': str(date_of_row),
+        "date_and_time": str(date_of_row),
         "garages": {},
     }
 
-    name_dict = {'0': 'A', '1': 'B', '2': 'C', '3': 'D', '4': 'H', '5': 'I', '6': 'Libra'}
+    name_dict = {
+        "0": "A",
+        "1": "B",
+        "2": "C",
+        "3": "D",
+        "4": "H",
+        "5": "I",
+        "6": "Libra",
+    }
     cnt += 1
 
-
-    for garage_number in range(7):
-        spaces_filled = int(float(int(data[3 * garage_number + 2])) - int(float(data[3 * garage_number + 1])))
+    for garage_number in range(len(name_dict.keys())):
+        spaces_filled = int(
+            float(int(data[3 * garage_number + 2])) - int(float(data[3 * garage_number + 1]))
+        )
         if spaces_filled < 0:
             spaces_filled = 0
-        formatted_dict_for_row_garage_number = {'max_spaces': int(float(data[3 * garage_number + 2])),
-                                                'percent_full': int(float(data[3 * garage_number + 3])), 'spaces_filled': spaces_filled ,
-                                                'spaces_left': int(float(data[3 * garage_number + 1]))}
 
+        formatted_dict_for_row_garage_number = {
+            "max_spaces": int(float(data[3 * garage_number + 2])),
+            "percent_full": int(float(data[3 * garage_number + 3])),
+            "spaces_filled": spaces_filled,
+            "spaces_left": int(float(data[3 * garage_number + 1])),
+        }
 
-        garage_row_queried['garages'][name_dict[str(garage_number)]] = (formatted_dict_for_row_garage_number)
+        garage_row_queried["garages"][  # type: ignore
+            name_dict[str(garage_number)]
+        ] = formatted_dict_for_row_garage_number
 
     return garage_row_queried, cnt
 
 
+class Database:
+    """Define the database interface."""
+
+    def __init__(self):
+        """Initialize the database."""
+        self.database = mysql.connector.connect(
+            host=os.environ["DB_HOST"],
+            port=os.environ["DB_PORT"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASS"],
+            database=os.environ["DB_NAME"],
+        )
+        self.cursor = self.database.cursor()
+
+    def setup_query_extractor(self):
+        """Set the query extractor."""
+        return DataQueryExtractor(self.database, self.cursor)
+
+    def update_database(self, query, data):
+        """Update the database."""
+        self.cursor.execute(query, data)
+        self.database.commit()
+
+    def close_connection(self):
+        """Close the connection to the database."""
+        self.database.close()
+        self.cursor.close()
+
+
 class DataQueryExtractor:
+    """Define functions to query data from the database."""
+
     def __init__(self, my_database, my_cursor):
+        """Initialize the class."""
         self.my_database = my_database
         self.my_cursor = my_cursor
 
+    def execute_query(self, query, *args):
+        """Execute a query on the database."""
+        self.my_cursor.execute(query, *args)
+        return self.my_cursor.fetchall()
 
     def stats(self):
-        query = "SELECT MAX(date_and_time) FROM parking_data"
-        self.my_cursor.execute(query)
-        for date in self.my_cursor:
+        """Get the stats of the database using SQL."""
+        query_result = self.execute_query("SELECT MAX(date_and_time) FROM parking_data")
+        for date in query_result:
             max_date = date[0]
 
-
-        query = "SELECT COUNT(*) FROM parking_data"
-        self.my_cursor.execute(query)
-        for count in self.my_cursor:
+        query_result = self.execute_query("SELECT COUNT(*) FROM parking_data")
+        for count in query_result:
             count = count[0]
 
         stats_data = {
-            'total_data_rows': count,
-            'last_date_and_time': str(max_date),
+            "total_data_rows": count,
+            "last_date_and_time": str(max_date),
         }
 
         return stats_data
 
-
     def get_lastday(self):
-        query = "SELECT * FROM parking_data ORDER BY date_and_time DESC LIMIT 24"
-        self.my_cursor.execute(query)
+        """Get the last day of data from the database using SQL."""
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data ORDER BY date_and_time DESC LIMIT 24"
+        )
 
         lastday_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            lastday_data_queried['data'].append(garage_row_queried)
+            lastday_data_queried["data"].append(garage_row_queried)
 
-        lastday_data_queried['count'] = cnt
+        lastday_data_queried["count"] = cnt
 
         return lastday_data_queried
 
-
-
     def get_lastmonth(self):
-        query = "SELECT * FROM parking_data ORDER BY date_and_time DESC LIMIT 720"
-        self.my_cursor.execute(query)
+        """Get the last month of data from the database using SQL."""
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data ORDER BY date_and_time DESC LIMIT 720"
+        )
 
         lastmonth_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            lastmonth_data_queried['data'].append(garage_row_queried)
+            lastmonth_data_queried["data"].append(garage_row_queried)
 
-        lastmonth_data_queried['count'] = cnt
+        lastmonth_data_queried["count"] = cnt
 
         return lastmonth_data_queried
 
-
-
     def get_lastyear(self):
-        query = "SELECT * FROM parking_data ORDER BY date_and_time DESC LIMIT 8760"
-        self.my_cursor.execute(query)
+        """Get the last year of data from the database using SQL."""
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data ORDER BY date_and_time DESC LIMIT 8760"
+        )
 
         lastyear_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            lastyear_data_queried['data'].append(garage_row_queried)
+            lastyear_data_queried["data"].append(garage_row_queried)
 
-        lastyear_data_queried['count'] = cnt
+        lastyear_data_queried["count"] = cnt
 
         return lastyear_data_queried
 
-
-
-
-
     def get_last_week_data(self):
+        """Get the last week of data from the database using SQL."""
         week_ago_date = datetime.datetime.now() - datetime.timedelta(days=7)
-        query = "SELECT * FROM parking_data WHERE date_and_time > %s ORDER BY date_and_time DESC"
-        self.my_cursor.execute(query, [week_ago_date])
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data WHERE date_and_time > %s ORDER BY date_and_time DESC",
+            [week_ago_date],
+        )
 
         weekly_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            weekly_data_queried['data'].append(garage_row_queried)
+            weekly_data_queried["data"].append(garage_row_queried)
 
-        weekly_data_queried['count'] = cnt
+        weekly_data_queried["count"] = cnt
 
         return weekly_data_queried
 
     def get_last_data(self):
-        query = "SELECT MAX(date_and_time) FROM parking_data"
-        self.my_cursor.execute(query)
-        for date in self.my_cursor:
+        """Get the last data from the database using SQL."""
+        query_result = self.execute_query("SELECT MAX(date_and_time) FROM parking_data")
+        for date in query_result:
             max_date = date[0]
 
-        query = "SELECT * FROM parking_data WHERE date_and_time = %s"
-        self.my_cursor.execute(query, [max_date])
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data WHERE date_and_time = %s", [max_date]
+        )
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
 
         return garage_row_queried
 
     def get_yearly_data(self, year):
-        query = "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s ORDER BY date_and_time DESC"
-        self.my_cursor.execute(query, [year])
+        """Get the year data from the database using SQL."""
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s ORDER BY date_and_time DESC",
+            [year],
+        )
 
         yearly_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            yearly_data_queried['data'].append(garage_row_queried)
+            yearly_data_queried["data"].append(garage_row_queried)
 
-        yearly_data_queried['count'] = cnt
+        yearly_data_queried["count"] = cnt
 
         return yearly_data_queried
 
     def get_yearly_monthly_data(self, year, month):
-        query = "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s AND EXTRACT(MONTH FROM date_and_time) = %s ORDER BY date_and_time DESC"
-        self.my_cursor.execute(query, [year, month])
+        """Get the yearly and monthly data from the database using SQL."""
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s AND EXTRACT(MONTH FROM date_and_time) = %s ORDER BY date_and_time DESC",
+            [year, month],
+        )
 
         yearly_monthly_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            yearly_monthly_data_queried['data'].append(garage_row_queried)
+            yearly_monthly_data_queried["data"].append(garage_row_queried)
 
-        yearly_monthly_data_queried['count'] = cnt
+        yearly_monthly_data_queried["count"] = cnt
 
         return yearly_monthly_data_queried
 
     def get_yearly_monthly_daily_data(self, year, month, the_day):
-        query = "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s AND EXTRACT(MONTH FROM date_and_time) = %s AND EXTRACT(DAY FROM date_and_time) = %s ORDER BY date_and_time DESC"
-        self.my_cursor.execute(query, [year, month, the_day])
+        """Get the yearly, monthly and daily data from the database using SQL."""
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s AND EXTRACT(MONTH FROM date_and_time) = %s AND EXTRACT(DAY FROM date_and_time) = %s ORDER BY date_and_time DESC",
+            [year, month, the_day],
+        )
 
         yearly_monthly_daily_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            yearly_monthly_daily_data_queried['data'].append(garage_row_queried)
+            yearly_monthly_daily_data_queried["data"].append(garage_row_queried)
 
-        yearly_monthly_daily_data_queried['count'] = cnt
+        yearly_monthly_daily_data_queried["count"] = cnt
 
         return yearly_monthly_daily_data_queried
 
     def get_all_data(self):
-        query = "SELECT * FROM parking_data ORDER BY date_and_time DESC"
-        self.my_cursor.execute(query)
-        result = self.my_cursor.fetchall()
+        """Get all the data from the database using SQL."""
+        query_result = self.execute_query("SELECT * FROM parking_data ORDER BY date_and_time DESC")
+
         all_data_queried = {
-            'count': 0,
-            'data': [0] * len(result),
+            "count": 0,
+            "data": [0] * len(query_result),
         }
 
         cnt = 0
 
-        for index, data in enumerate(result):
+        for index, data in enumerate(query_result):
             garage_row_queried, cnt = row_formatting(data, cnt)
-            all_data_queried['data'][index] = garage_row_queried
+            all_data_queried["data"][index] = garage_row_queried
 
-        all_data_queried['count'] = cnt
+        all_data_queried["count"] = cnt
 
         return all_data_queried
 
-
-
-
     def get_today_data(self):
+        """Get the today data from the database using SQL."""
         today_date = datetime.datetime.now()
         today_date = str(today_date)
-        year = today_date.split('-')[0]
-        month = today_date.split('-')[1]
-        day = today_date.split('-')[2]
-        query = "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s AND EXTRACT(MONTH FROM date_and_time) = %s AND EXTRACT(DAY FROM date_and_time) = %s ORDER BY date_and_time DESC"
-        self.my_cursor.execute(query, [year, month, day])
+        year = today_date.split("-")[0]
+        month = today_date.split("-")[1]
+        day = today_date.split("-")[2]
+        query_result = self.execute_query(
+            "SELECT * FROM parking_data WHERE EXTRACT(YEAR FROM date_and_time) = %s AND EXTRACT(MONTH FROM date_and_time) = %s AND EXTRACT(DAY FROM date_and_time) = %s ORDER BY date_and_time DESC",
+            [year, month, day],
+        )
 
         all_today_data_queried = {
-            'count': 0,
-            'data': [],
+            "count": 0,
+            "data": [],
         }
 
         cnt = 0
-        for data in self.my_cursor:
+        for data in query_result:
             garage_row_queried, cnt = row_formatting(data, cnt)
-            all_today_data_queried['data'].append(garage_row_queried)
+            all_today_data_queried["data"].append(garage_row_queried)
 
-        all_today_data_queried['count'] = cnt
+        all_today_data_queried["count"] = cnt
 
         return all_today_data_queried
 
-
     def get_all_data_for_predictions(self):
-        query = "SELECT * FROM parking_data ORDER BY date_and_time DESC"
-        self.my_cursor.execute(query)
-        result = self.my_cursor.fetchall()
+        """Get all the data from the database using SQL for prediction purposes."""
+        query_result = self.execute_query("SELECT * FROM parking_data ORDER BY date_and_time DESC")
 
-
-        return result
+        return query_result
